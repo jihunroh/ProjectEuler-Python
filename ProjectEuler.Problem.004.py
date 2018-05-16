37504b9cdf8f122a095cdbf7601464aa031e3bd6
jihunroh <jihunroh@outlook.kr>
2018-05-16 14:35:03 +0900

    4번 풀이

--- ProjectEuler.Problem.004.py
+++ ProjectEuler.Problem.004.py
@@ -1,23 +1,12 @@
-import time
-T0 = time.clock()
+from ProjectEulerCommon import Answer
 
-def is_PalindromicNumber(n):
+def is_palindromic(n):
     n = str(n)
     for i in range(0,len(n)):
         if not n[i] == n[len(n) - i - 1]:
              return False
     return True
 
-ans=[]
-x = list(range(100, 1000))
-x.reverse()
-y = x
-
-for i in x:
-    for j in y:
-        if is_PalindromicNumber(i * j):
-            ans.append(i * j)
-ans = list(ans)
-ans.sort()
-print('Answer is', ans[-1])
-print('The execution time is', time.clock()-T0)
+Answer(
+    max([x * y  for x in range(100,1000) for y in range(100,1000) if is_palindromic(x * y)])
+)
