da7fc39e8068aaeb122c0e72a72b354c947ff111
jihunroh <jihunroh@outlook.kr>
2018-05-17 12:57:12 +0900

    문제 7 풀이, is_prime 오류 수정

--- PrimeNumbers.py
+++ PrimeNumbers.py
@@ -1,30 +1,27 @@
 from math import sqrt
+from itertools import islice
 
 def is_prime(x):
+    if x == 2:
+        return True
     if x % 2 == 0:
         return False
-    if x % 3 == 0:
-        return False
-    if x % 5 == 0:
-        return False
-    if x % 7 == 0:
-        return False
-    if x % 11 == 0:
-        return False
-    if x % 13 == 0:
-        return False
-    for divisor in [a for a in range(2, int(sqrt(x)) + 1) if not a % 2 == 0 and not a % 3 == 0 and not a % 5 == 0 and not a % 7 == 0]:
+    for divisor in [a for a in range(2, int(sqrt(x)) + 1) if not a % 2 == 0]:
         if x % divisor == 0:
             return False
     return True
 
-def iterate_prime():
+def generate_prime():
     n = 2
     yield n
     n = 3
     yield n
-
+    
     while True:
         n += 2
         if is_prime(n):
             yield n
+
+def get_nth_prime(n):
+    f = generate_prime()
+    return list(islice(f, n))[-1]
--- ProjectEuler.Problem.007.py
+++ ProjectEuler.Problem.007.py
@@ -1,25 +1,6 @@
-from math import sqrt
+from ProjectEulerCommon import Answer
+from PrimeNumbers import get_nth_prime
 
-def is_Prime(x):
-    if x == 1:
-        return False
-    if x == 2 or x == 3 or x == 5 or x == 7 or x == 11:
-        return True
-    if x % 2 == 0 or x % 3 == 0 or x % 5 == 0 or x % 7 == 0 or x % 11 == 0:
-        return False
-    for divisor in [a for a in range(2, int(sqrt(x)) + 1) if not a % 2 == 0]:
-        if x % divisor == 0:
-            return False
-    return True
-
-def getNthPrime(n):
-    Number, i = 3, 2
-    while not i == n + 1:
-        if is_Prime(Number):
-            i += 1
-        else:
-            pass
-        Number += 2
-    return Number - 2
-
-print(getNthPrime(10001))
+Answer(
+    get_nth_prime(10001)
+)
