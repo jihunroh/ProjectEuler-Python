8e3d7f032a9084b754aaf830a569273e2a8c18c2
jihunroh <jihunroh@outlook.kr>
2018-05-17 17:56:10 +0900

    12번 풀이

--- ProjectEuler.Problem.012.py
+++ ProjectEuler.Problem.012.py
@@ -1,28 +1,11 @@
-from math import sqrt
-import time
+from ProjectEulerCommon import Answer
+from Factors import factorize
+from itertools import count
 
-def Factorize(n):
-    FactorsList = [1, n]
-    for divisor in [a for a in range(2, int(sqrt(n)) + 1)]:
-        if n % divisor == 0:
-            FactorsList.append(divisor)
-            if (int(n / divisor) != divisor):
-                FactorsList.append(int(n / divisor))
-    FactorsList.sort()
-    return FactorsList
+def generate_triangular_number():
+    for i in count(1):
+        yield int(i* (i+1) * 0.5)
 
-def TriangularNumber(n):
-    return sum(range(1,n+1))
-
-T0 = time.clock()
-length, i = 0, 1
-
-while length < 500:
-    length = len(Factorize(TriangularNumber(i))) - 1
-    i += 1
-print(TriangularNumber(i-1))
-print('The execution time is', time.clock()-T0)
-"""
-76576500
-The execution time is 29.4996305969
-"""
+Answer(
+    next(x for x in generate_triangular_number() if len(factorize(x)) > 500)
+)
