c1a23671c23d5ce07fbbd9f6df77e1d97709c76f
jihunroh <jihunroh@outlook.kr>
2018-05-17 13:40:15 +0900

    9번 풀이

--- ProjectEuler.Problem.009.py
+++ ProjectEuler.Problem.009.py
@@ -1,8 +1,5 @@
-def PythagoreanTreiplet():
-    for a in range(1, 1000):
-        for b in range(1, 1000):
-            c = 1000 - a - b
-            if a * a + b * b == c * c:
-                return a * b * c
+from ProjectEulerCommon import Answer
 
-print(PythagoreanTreiplet())
+Answer(
+    [a * b * (1000 - a - b) for a in range(1, 1000) for b in range(1, 1000) if a * a + b * b == (1000 - a - b) * (1000 - a - b)][0]
+)
