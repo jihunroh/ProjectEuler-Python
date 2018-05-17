bf366c52157fd113950bcb181a0061af9d768b28
jihunroh <jihunroh@outlook.kr>
2018-05-17 12:11:07 +0900

    5번 풀이

--- Factors.py
+++ Factors.py
@@ -9,3 +9,33 @@ def factorize(n):
                 FactorsList.append(int(n / divisor))
     FactorsList.sort()
     return FactorsList
+
+def prime_factorize(n):
+    divisor = 2
+    prime_factor_dict = {}
+    
+    while not n == 1:
+        if n % divisor == 0:
+            try:
+                prime_factor_dict[divisor] += 1
+            except KeyError:
+                prime_factor_dict[divisor] = 1
+            n = n / divisor
+        else:
+            divisor += 1
+    return prime_factor_dict
+
+def get_LCM(number_list):
+    factors, result = {}, 1
+    prime_factors_dict_list = [prime_factorize(x) for x in number_list]
+    
+    for prime_factors_dict in prime_factors_dict_list:
+        for factor in prime_factors_dict.keys():
+            factors[factor] = max([prime_factors_dict2.get(factor, 0) for prime_factors_dict2 in prime_factors_dict_list])
+    
+    for factor, power in factors.items():
+        result *= pow(factor, power)
+    return result
+
+def get_GCD(numberlist):
+    return int(prod(numberlist) / getLCM(numberlist))
--- ProjectEuler.Problem.005.py
+++ ProjectEuler.Problem.005.py
@@ -1,48 +1,6 @@
-from math import sqrt
+from ProjectEulerCommon import Answer
+from Factors import get_LCM
 
-def Factorize(n):
-    FactorsList = [1, n]
-    for divisor in [a for a in range(2, int(sqrt(n)) + 1)]:
-        if n % divisor == 0:
-            FactorsList.append(divisor)
-            if (int(n / divisor) != divisor):
-                FactorsList.append(int(n / divisor))
-    FactorsList.sort()
-    return FactorsList
-
-def DecimalNotation(n):
-    divisor = 2
-    DecimalNotationList = {}
-    
-    while not n == 1:
-        if n % divisor == 0:
-            try:
-                DecimalNotationList[divisor] += 1
-            except KeyError:
-                DecimalNotationList[divisor] = 1
-            n = n / divisor
-        else:
-            divisor += 1
-    return DecimalNotationList
-
-def getGCDLCM(Type, NumberList):
-    NumberDecList, Factors, Result = [], {}, 1
-    
-    for x in NumberList:
-        NumberDecList.append(DecimalNotation(x))
-
-    for entry_i in NumberDecList:
-        for key in entry_i.keys():
-            Temp = []
-            for entry_j in NumberDecList:
-                try:
-                    Temp.append(entry_j.get(key, 0))
-                    Factors[key] = Type == 'GCD' and min(Temp)or max(Temp)
-                except:
-                    pass
-    
-    for factor, power in Factors.items():
-        Result *= pow(factor, power)
-    return Result
-
-print(getGCDLCM('LCM', range(1,21)))
+Answer(
+    get_LCM(range(1,20+1))
+)
