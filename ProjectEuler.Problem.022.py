d60ddbc6460731934e9cf7473cbb682cb507fe66
jihunroh <jihunroh@outlook.kr>
2018-05-20 00:29:40 +0900

    22번 풀이

--- ProjectEuler.Problem.022.py
+++ ProjectEuler.Problem.022.py
@@ -1,19 +1,41 @@
-def alphabeticalValue(c):
-    return ord(c) - 64
+from ProjectEulerCommon import Answer
 
-raw_data = open('names.txt')
-for data in raw_data:
-    pass
-data = data.replace('"', '')
-data = data.split(',')
-data.sort()
-score = alphascore = 0
-#data = ['AAA','BBB', 'CCC']
+alphabet2order = {
+    'A': 1,
+    'B': 2,
+    'C': 3,
+    'D': 4,
+    'E': 5,
+    'F': 6,
+    'G': 7,
+    'H': 8,
+    'I': 9,
+    'J': 10,
+    'K': 11,
+    'L': 12,
+    'M': 13,
+    'N': 14,
+    'O': 15,
+    'P': 16,
+    'Q': 17,
+    'R': 18,
+    'S': 19,
+    'T': 20,
+    'U': 21,
+    'V': 22,
+    'W': 23,
+    'X': 24,
+    'Y': 25,
+    'Z': 26
+}
 
-for x in data:
-    for y in x: #낱개 글자
-        alphascore += alphabeticalValue(y)
-    orderscore = data.index(x) + 1
-    score += alphascore * orderscore
-    alphascore = orderscore = 0
-print(score)
+def alphabet_order_sum(name):
+    return sum([alphabet2order[alphabet] for alphabet in name])
+
+with open('ProjectEuler.Problem.022.names.txt', 'r') as f:
+    names = [line.replace('"', '').split(',') for line in f.readlines()][0]
+names.sort()
+
+Answer(
+    sum([(i + 1) * alphabet_order_sum(name) for i, name in enumerate(names)])
+)
