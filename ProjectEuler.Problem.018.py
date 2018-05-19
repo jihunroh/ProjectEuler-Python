59e2e808b8ff2c5c65fc7573076989b23fd98b11
jihunroh <jihunroh@outlook.kr>
2018-05-19 22:58:07 +0900

    18번 풀이

--- ProjectEuler.Problem.018.py
+++ ProjectEuler.Problem.018.py
@@ -1,93 +1,27 @@
-import time
-T0 = time.clock()
-def num2char(n):
-    if n == 1:
-        return 'One'
-    elif n == 2:
-        return 'Two'
-    elif n == 3:
-        return 'Three'
-    elif n == 4:
-        return 'Four'
-    elif n == 5:
-        return 'Five'
-    elif n == 6:
-        return 'Six'
-    elif n == 7:
-        return 'Seven'
-    elif n == 8:
-        return 'Eight'
-    elif n == 9:
-        return 'Nine'
-    elif n == 10:
-        return 'Ten'
-    elif n == 20:
-        return 'Twenty'
-    elif n == 30:
-        return 'Thirty'
-    elif n == 40:
-        return 'Forty'
-    elif n == 50:
-        return 'Fifty'
-    elif n == 60:
-        return 'Sixty'
-    elif n == 70:
-        return 'Seventy'
-    elif n == 80:
-        return 'Eighty'
-    elif n == 90:
-        return 'Ninety'
-    elif n == 100:
-        return 'Hundred'
-    elif n == 11:
-        return 'Eleven'
-    elif n == 12:
-        return 'Twelve'
-    elif n == 13:
-        return 'Thirteen'
-    elif n == 14:
-        return 'Fourteen'
-    elif n == 15:
-        return 'Fifteen'
-    elif n == 16:
-        return 'Sixteen'
-    elif n == 17:
-        return 'Seventeen'
-    elif n == 18:
-        return 'Eighteen'
-    elif n == 19:
-        return 'Nineteen'
-    elif n == 1000:
-        return 'Thousand'
+from ProjectEulerCommon import Answer
+triangle = [list(map(int, line.split(' '))) + [0] * (15 - 1 - i) for i, line in enumerate(
+"""75
+95 64
+17 47 82
+18 35 87 10
+20 04 82 47 65
+19 01 23 75 03 34
+88 02 77 73 07 63 67
+99 65 04 28 06 16 70 92
+41 41 26 56 83 40 80 70 33
+41 48 72 33 47 32 37 16 94 29
+53 71 44 65 25 43 91 52 97 51 14
+70 11 33 28 77 73 17 78 39 68 17 57
+91 71 52 38 17 14 91 43 58 50 27 29 48
+63 66 04 68 89 53 67 30 73 16 69 87 40 31
+04 62 98 27 23 09 70 98 73 93 38 53 60 04 23""".splitlines())]
+
+def reduce_to_local_maximum_path(triangle):
+    if len(triangle) == 1:
+        return triangle[0][0]
     else:
-        return None
+        return reduce_to_local_maximum_path(triangle[0:len(triangle) - 2] + [[step + max(triangle[-1][i:i+2]) for i, step in enumerate(triangle[-2]) if step is not 0]])
 
-def num2phrase(n):
-    ## 1000의 자리
-    phrase = num2char(int(n / 1000)) + num2char(1000) if int(n / 1000) != 0 else ''
-    n = n - int(n / 1000) * 1000
-
-    ## 100의 자리
-    phrase += num2char(int(n / 100)) + num2char(100) if int(n / 100) != 0 else ''
-    
-    ## and 처리
-    phrase += 'And' if int(n / 100) != 0 and n % 100 != 0 else ''
-    n = n - int(n / 100) * 100 
-        
-    ## 10의 자리
-    if int(n / 10) == 1:
-        phrase += num2char(n)
-    else:
-        phrase += num2char(int(n / 10) * 10) if int(n / 10) != 0 else '' 
-    n = n - int(n / 10) * 10 if int(n / 10) != 1 else 0
-
-    ## 1의 자리
-    phrase += num2char(n) if n != 0 else ''
-    return phrase
-length = 0
-
-for i in range(1,1001):
-    data = num2phrase(i)
-    length += len(data) 
-print(length)
-print('The execution time is', time.clock()-T0)
+Answer(
+    reduce_to_local_maximum_path(triangle)
+)
