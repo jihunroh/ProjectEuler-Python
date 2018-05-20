45ca7f7aa1cf7a94e28486124619cabe9fd1e501
jihunroh <jihunroh@outlook.kr>
2018-05-20 10:20:25 +0900

    23번 풀이

--- ProjectEuler.Problem.023.py
+++ ProjectEuler.Problem.023.py
@@ -1,30 +1,8 @@
-import time
-from math import sqrt
+from ProjectEulerCommon import Answer
+from Factors import factorize
 
-T0 = time.clock()
-def factorize(n):
-    FactorsList = set([1])
-    for divisor in [a for a in range(2, int(sqrt(n)) + 1)]:
-        if n % divisor == 0:
-            FactorsList.add(divisor)
-            FactorsList.add(int(n / divisor))
-            #FactorsList.append(divisor)
-            #FactorsList.append(int(n / divisor))
-    return FactorsList
+list_abundant = [n for n in range(1, 28123 + 1) if sum(factorize(n)[0:-1]) > n]
 
-def is_abundant(n):
-    return sum(factorize(n)) > n
-def list_abundants():
-    return [x for x in range(1, 28124) if is_abundant(x)] #20161
-def possible_sums_of_two_abundant(abundantList):
-    total = set([])
-    for i in range(len(abundantList)):
-        for j in range(i, len(abundantList)):
-            s = abundantList[i]+abundantList[j]
-            if s < 28124:
-                total.add(s)
-    return total
-print(sum(range(1, 28124)) - sum(possible_sums_of_two_abundant(list_abundants())))
-print('The execution time is', time.clock()-T0)
-#4179871
-#The execution time is 13.2483100259
+print(
+    sum(set(range(1, 28123 + 1)) - set([a + b for a in list_abundant for b in list_abundant if a <= b and a + b <= 28123]))
+)
