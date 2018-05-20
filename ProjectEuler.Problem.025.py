7174f573bad738b7d8edb1afe059e24ad2e4b50b
jihunroh <jihunroh@outlook.kr>
2018-05-20 16:37:18 +0900

    25번 풀이

--- ProjectEuler.Problem.025.py
+++ ProjectEuler.Problem.025.py
@@ -1,18 +1,6 @@
-import time
+from ProjectEulerCommon import Answer
+from FibonacciSequence import generate_fibonacci
 
-def NextFibonacci(terms):
-    return sum(terms)
-
-terms, length, i = [1, 1], 1, 2 
-T0 = time.clock()
-
-while not length == 1000:
-    next = NextFibonacci(terms) 
-    terms.pop(0)
-    terms.append(next)
-    length = len(str(terms[1]))
-    i += 1
-    
-print(terms[1], i)    
-
-print('The execution time is', time.clock()-T0)
+Answer(
+    next(i for i, n in enumerate(generate_fibonacci()) if len(str(n)) == 1000) + 1
+)
