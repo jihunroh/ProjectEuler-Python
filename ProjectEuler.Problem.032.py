ea8a6ac0d2031385e9c38103c263e3c283bb0fd4
jihunroh <jihunroh@outlook.kr>
2018-05-21 17:37:45 +0900

    32번 풀이

--- ProjectEuler.Problem.032.py
+++ ProjectEuler.Problem.032.py
@@ -1,6 +1,20 @@
-import time
-from eulerlibs import factorial
-T0 = time.clock()
-print(factorial(9))
+from ProjectEulerCommon import Answer
+from itertools import permutations
 
-print('The execution time is', time.clock()-T0)
+def canbe_equation(pandigital, product_length):
+    product = int(''.join(map(str, pandigital[9 - product_length:])))
+    for multiplicand_length in range(1, 9 - product_length):
+        multiplicand = int(''.join(map(str, pandigital[:multiplicand_length])))
+        multiplier   = int(''.join(map(str, pandigital[multiplicand_length:9 - product_length])))
+        if product == multiplicand * multiplier:
+            return True
+    return False
+
+Answer(
+    sum(set([
+        int(''.join(map(str, pandigital[9 - product_length:])))
+        for product_length in range(4, 5)
+        for pandigital in permutations(range(1, 10))
+        if canbe_equation(pandigital, product_length)
+    ]))
+)
