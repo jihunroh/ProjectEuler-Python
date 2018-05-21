2a07a3f8076b37fb75743859580a9c8f53d80877
jihunroh <jihunroh@outlook.kr>
2018-05-22 08:24:16 +0900

    33번 풀이

--- ProjectEuler.Problem.033.py
+++ ProjectEuler.Problem.033.py
@@ -1,19 +1,12 @@
-import time
-from eulerlibs import prod, getGCD
-T0 = time.clock()
-nums, dens = [], []
-for numerator in range(10, 100):
-    for denominator in range(numerator + 1, 100):
-        for n in str(numerator).replace('0', ''):
-            if n in str(denominator):
-                num, den = int(str(numerator).replace(n, '', 1)), int(str(denominator).replace(n, '', 1))
-                if den == 0:
-                    continue
-                if num / den == numerator / denominator :
-                    nums.append(numerator)
-                    dens.append(denominator)
-GCD = getGCD([prod(nums), prod(dens)])
-print(int(prod(dens) / GCD))
-print('The execution time is', time.clock()-T0)
-#100
-#The execution time is 0.0247488906133
+from ProjectEulerCommon import Answer
+from ProjectEulerCommon import product
+from Factors import get_irreducibe_fraction
+
+Answer(
+    get_irreducibe_fraction(
+    [product(bulator) for bulator in list(zip(*[[numerator, denominator] for numerator in range(10, 100) for denominator in range(10, 100)
+    if numerator < denominator and
+    ((denominator * int(str(numerator)[1]) == numerator * int(str(denominator)[0]) and str(denominator)[1] == str(numerator)[0]) or
+    (denominator * int(str(numerator)[0]) == numerator * int(str(denominator)[1]) and str(denominator)[0] == str(numerator)[1]))]))]
+    )[1]
+)
