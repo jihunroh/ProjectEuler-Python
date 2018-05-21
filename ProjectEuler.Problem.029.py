c9d9dd830739978eb5bd4fe32bb8e28e2e4f41b0
jihunroh <jihunroh@outlook.kr>
2018-05-21 11:32:41 +0900

    29번 풀이

--- ProjectEuler.Problem.029.py
+++ ProjectEuler.Problem.029.py
@@ -1,16 +1,5 @@
-import time
-T0 = time.clock()
+from ProjectEulerCommon import Answer
 
-def append_and_remove_duplicated(List, item):
-    if not List.count(item) == 0:
-        List.remove(item)  
-    List.append(item)
-    return List
-Total = []
-
-for a in range(2, 101):
-    for b in range(2, 101):
-        append_and_remove_duplicated(Total, a**b)
-Total.sort()
-print('Answer is', len(Total))
-print('The execution time is', time.clock()-T0)
+Answer(
+    len(sorted(list(set([a**b for a in range(2, 100 + 1) for b in range(2, 100 + 1)]))))
+)
