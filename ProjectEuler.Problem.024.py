46c799301c44e91d55b4d1ef912445d6fa651f8c
jihunroh <jihunroh@outlook.kr>
2018-05-21 16:29:45 +0900

    23번 속도 개선

--- ProjectEuler.Problem.024.py
+++ ProjectEuler.Problem.024.py
@@ -1,13 +1,6 @@
 from ProjectEulerCommon import Answer
-
-def generate_permutations(digit_lists):
-    if len(digit_lists) == 1:
-        yield digit_lists
-    else:
-        for i, digit in enumerate(digit_lists):
-            for sub_permutation in generate_permutations(digit_lists[:i] + digit_lists[i+1:]):
-                yield [digit] + sub_permutation
+from itertools import permutations
 
 Answer(
-    ''.join([str(digit) for digit in next(n for i, n in enumerate(generate_permutations(list(range(10)))) if i == 1000000 - 1)])
+    ''.join([str(digit) for digit in next(n for i, n in enumerate(permutations(list(range(10)))) if i == 1000000 - 1)])
 )
