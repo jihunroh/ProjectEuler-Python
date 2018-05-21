ef362e93d669a2947f942cf3857dd034d0c9d7e4
jihunroh <jihunroh@outlook.kr>
2018-05-21 11:29:32 +0900

    28번 풀이

--- ProjectEuler.Problem.028.py
+++ ProjectEuler.Problem.028.py
@@ -1,22 +1,17 @@
-def leftup(n):
+from ProjectEulerCommon import Answer
+
+def leftup(n): # 1, 7, 21, 43
     return  4 * n ** 2 - 6 * n + 3
-    # 1, 7, 21, 43
 
-def leftdown(n):
+def leftdown(n): # 1, 5, 17, 37
     return  4 * n ** 2 - 8 * n + 5
-    # 1, 5, 17, 37
 
-def rightup(n):
+def rightup(n): # 1, 9, 25, 49
     return  4 * n ** 2 - 4 * n + 1
-    # 1, 9, 25, 49
 
-def rightdown(n):
+def rightdown(n): # 1, 3, 13, 31
     return  4 * n ** 2 - 10 * n + 7
-    # 1, 3, 13, 31
 
-def diagonalsum(n): # n is square matrix dimension
-    ans = 1
-    for i in range(2, int(n / 2) + 2):
-        ans += leftup(i) + leftdown(i) + rightup(i) + rightdown(i)
-    return ans
-print(diagonalsum(1001))
+Answer(
+    1 + sum([sum([leftup(i), leftdown(i), rightup(i), rightdown(i)]) for i in range(2, int(1001 / 2) + 2)])
+)
