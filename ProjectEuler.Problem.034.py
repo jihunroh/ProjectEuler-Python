a792f368c2dad4c176377e66536d1f2269f70093
jihunroh <jihunroh@outlook.kr>
2018-05-22 09:28:17 +0900

    34번 풀이

--- ProjectEuler.Problem.034.py
+++ ProjectEuler.Problem.034.py
@@ -1,62 +1,17 @@
-import time
-T0 = time.clock()
+from ProjectEulerCommon import Answer
+from ProjectEulerCommon import factorial
+from itertools import count
 
-def factorial(n):
-    if n == 0:
-        return 1
-    elif n == 1:
-        return 1
-    elif n == 2:
-        return 2
-    elif n == 3:
-        return 6
-    elif n == 4:
-        return 24
-    elif n == 5:
-        return 120
-    elif n == 6:
-        return 720
-    elif n == 7:
-        return 5040
-    elif n == 8:
-        return 40320
-    elif n == 9:
-        return 362880
+Answer(
+    sum([n for n in range(10, factorial(9) * (next(digits_length for digits_length in count(1) if factorial(9) * digits_length < 10**(digits_length - 1)) - 1)) if  sum([factorial(int(digit)) for digit in str(n)]) == n])
+)
 
-def sum_of_factorials_of_digits(n):
-    n, sum = str(n), 0
-
-    if int(n) < 362880 * list(n).count('9'): #9! = 362880
-        return False
-    if int(n) < 40320 * list(n).count('8'):
-        return False
-    if int(n) < 5040 * list(n).count('7'):
-        return False
-    if int(n) < 720 * list(n).count('6'):
-        return False
-
-    for digit in n:
-        digit = int(digit)
-        sum += factorial(digit)
-
-    if sum == int(n):
-        return True
-    return False
-Total = []
-for i in range(3, 1000000):
-    if (sum_of_factorials_of_digits(i)):
-        Total.append(i)
-print(sum(Total))
-print('The execution time is', time.clock()-T0)
-
-print([factorial(x) for x in range(1,10)])
-# 계산 자릿수 계산
-#number = ''
-#for n in range(1,11):
-#    for i in range(n):
-#        number += '1'
-#    print(n * 362880, int(number)* 9)
-#    if (int(number) * 9 > n * 362880):
-#        break
-#    number = ''
-#print(n)
+# 숫자                  자릿수팩토리얼 합
+# 1~9			        1~362,880	    1	    9
+# 10~99			        2~725,76        10      99
+# 100~999 		    	3~1,088,640     100     999
+# 1,000~9,999	    	4~1,451,520     1000	9999
+# 10,000~99,999	    	5~1,814,400 	10000	99999
+# 100,000~999,999	    6~2,177,280 	100000	999999
+# 1,000,000~9,999,999	7~2,540,160     1000000	2540160
+# 10,000,000~99,999,999	8~2,903,040     x       x
