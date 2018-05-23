65a839d48a943c5adaa7258b26e7faa235dee95a
jihunroh <jihunroh@outlook.kr>
2018-05-23 22:23:01 +0900

    45번 풀이

--- ProjectEuler.Problem.045.py
+++ ProjectEuler.Problem.045.py
@@ -1,36 +1,6 @@
-import time
-from math import sqrt
-T0 = time.clock()
+from ProjectEulerCommon import Answer
+from GeometricalNumbers import generate_triangular_number, is_hexagonal, is_pentagonal
 
-def is_integer(a):
-    return a % 1 == 0
-
-#def is_triangular(n):
-#    return is_integer((-1 + sqrt(1 + 8 * n)) / 2)
-
-def is_pentagonal(n):
-    return is_integer((1 + sqrt(1 + 24 * n)) / 6)
-   
-def is_hexagonal(n):
-    return is_integer((1 + sqrt(1 + 8 * n)) / 4)
-
-def hexagonal():
-    i = 1
-    while True:
-        yield i * (2 * i - 1)
-        i += 1
-
-def iterate_seq():
-    while True:
-        for n in hexagonal():
-            if is_pentagonal(n):
-                yield n
-f = iterate_seq()
-print(next(f))
-print(next(f))
-print(next(f))
-#1
-#40755
-#1533776805
-#The execution time is 0.112061200221
-print('The execution time is', time.clock()-T0)
+Answer(
+    next(x for x in generate_triangular_number() if x > 40755 and is_hexagonal(x) and is_pentagonal(x))
+)
