af2045e9b5af671682813a7336ba6ef36db90111
jihunroh <jihunroh@outlook.kr>
2018-05-23 22:22:45 +0900

    GeometricalNumbers 라이브러리 추가

---
+++ GeometricalNumbers.py
@@ -0,0 +1,15 @@
+from math import sqrt
+from itertools import count
+
+def generate_triangular_number():
+    for i in count(1):
+        yield int(i* (i+1) * 0.5)
+
+def is_triangular(n):
+   return ((-1 + sqrt(1 + 8 * n)) / 2).is_integer()
+
+def is_pentagonal(n):
+    return ((1 + sqrt(1 + 24 * n)) / 6).is_integer()
+   
+def is_hexagonal(n):
+    return ((1 + sqrt(1 + 8 * n)) / 4).is_integer()
--- ProjectEuler.Problem.012.py
+++ ProjectEuler.Problem.012.py
@@ -1,10 +1,7 @@
 from ProjectEulerCommon import Answer
 from Factors import factorize
 from itertools import count
-
-def generate_triangular_number():
-    for i in count(1):
-        yield int(i* (i+1) * 0.5)
+from GeometricalNumbers import generate_triangular_number
 
 Answer(
     next(x for x in generate_triangular_number() if len(factorize(x)) > 500)
