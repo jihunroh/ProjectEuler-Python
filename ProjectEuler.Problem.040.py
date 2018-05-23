1ff3c394288433d9ce1a638eb63680252a5582f9
jihunroh <jihunroh@outlook.kr>
2018-05-23 13:24:26 +0900

    40번 풀이

--- ProjectEuler.Problem.040.py
+++ ProjectEuler.Problem.040.py
@@ -1,23 +1,17 @@
-import time
-T0 = time.clock()
-def iterate_number():
-    i = 1
-    while True:
-        yield i
-        i += 1
+from ProjectEulerCommon import Answer
+from itertools import count
 
-def iterate_decimal_fraction():
-    g = iterate_number()
-    while True:
-        n = str(next(g))
-        for i in range(len(n)):
-            yield n[i]
+def generate_digit_of_irrational_decimal_fraction():
+    for i in count(1):
+        for j in str(i):
+            yield int(j)
 
-f, product = iterate_decimal_fraction(), 1
-
-for i in range(1, 1000000 + 1):
-    seq = next(f)
-    if i == 1 or i == 10 or i == 100 or i == 1000 or i == 10000 or i == 100000 or i == 1000000:
-        product *= int(seq)
-print(product)
-print('The execution time is', time.clock()-T0)
+Answer(
+    next(x for i, x in enumerate(generate_digit_of_irrational_decimal_fraction()) if i + 1 == 1) *
+    next(x for i, x in enumerate(generate_digit_of_irrational_decimal_fraction()) if i + 1 == 10) *
+    next(x for i, x in enumerate(generate_digit_of_irrational_decimal_fraction()) if i + 1 == 100) *
+    next(x for i, x in enumerate(generate_digit_of_irrational_decimal_fraction()) if i + 1 == 1000) *
+    next(x for i, x in enumerate(generate_digit_of_irrational_decimal_fraction()) if i + 1 == 10000) *
+    next(x for i, x in enumerate(generate_digit_of_irrational_decimal_fraction()) if i + 1 == 100000) *
+    next(x for i, x in enumerate(generate_digit_of_irrational_decimal_fraction()) if i + 1 == 1000000)
+)
