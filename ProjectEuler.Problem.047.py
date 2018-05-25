2ed6bd299306ba30fa09139bbf2418c7f07e14e6
jihunroh <jihunroh@outlook.kr>
2018-05-25 20:59:22 +0900

    47번 풀이

--- ProjectEuler.Problem.047.py
+++ ProjectEuler.Problem.047.py
@@ -1,23 +1,13 @@
-import time
-from eulerlibs import factorize, prime_factorize
-T0 = time.clock()
-digits = 4
+from ProjectEulerCommon import Answer
+from Factors import prime_factorize
+from itertools import count
 
-def iterate_number():
-    n = 1
-    while True:
-        for i in range(digits):
-            if not len(prime_factorize(n+i)) == digits:
-                n = n + i + 1
-                break
-            elif i == digits - 1:
-                yield(n)
-            else:
-                continue
-        
-g = iterate_number()
-print(next(g))
-#print(prime_factorize(1))
-print('The execution time is', time.clock()-T0)
-#134043
-#The execution time is 25.6277454857
+Answer(
+    next(num for num in count(3)
+    if range(num, num + 4) and
+    len(prime_factorize(num)) == 4 and
+    len(prime_factorize(num + 1)) == 4 and
+    len(prime_factorize(num + 2)) == 4 and
+    len(prime_factorize(num + 3)) == 4
+    )
+)
