e657d9addfc8a80726baf8f5b2963a277a15a241
jihunroh <jihunroh@outlook.kr>
2018-05-27 12:20:59 +0900

    49번 풀이

--- ProjectEuler.Problem.049.py
+++ ProjectEuler.Problem.049.py
@@ -1,26 +1,14 @@
-from ProjectEulerCommon import Answer
-from ProjectEulerCommon import first_true
-from Factors import prime_factorize
-from itertools import count
-
-def find_consecutive_nums():
-    n = 3
-
-    while True:
-        if len(prime_factorize(n)) == 4:
-            if len(prime_factorize(n + 1)) == 4:
-                if len(prime_factorize(n + 2)) == 4:
-                    if len(prime_factorize(n + 3)) == 4:
-                        return (n)
-                    else:
-                        n += 4
-                else:
-                    n += 3
-            else:
-                n += 2
-        else:
-            n += 1
+from ProjectEulerCommons.Base import *
+from ProjectEulerCommons.PrimeNumbers import is_prime
 
 Answer(
-    find_consecutive_nums()
+    nth((''.join(map(str, [n, n + d, n + 2 * d])) for n in range(1487 + 2, 10000, 2) for d in range(2, int((10000 - n) * 0.5), 2) if is_prime(n) and is_prime(n + d) and is_prime(n + 2 * d) and sorted(str(n)) == sorted(str(n + d)) == sorted(str(n + 2 * d))), 0)
 )
+
+"""
+------------------------------------------------
+   ProjectEuler.Problem.049.py
+   The Answer is: 296962999629
+   Time Elasped: 8.519287586212158sec
+------------------------------------------------
+"""
