7af849c9274d3309e7da97f858aceda404b40155
jihunroh <jihunroh@outlook.kr>
2018-05-27 19:35:39 +0900

    56번 풀이

--- ProjectEuler.Problem.056.py
+++ ProjectEuler.Problem.056.py
@@ -1,16 +1,13 @@
-import time
-T0 = time.clock()
-def num2digitallist(n):
-    digitallist = list(str(n))
-    for i in range(len(digitallist)):
-        digitallist[i] = int(digitallist[i])
-    return digitallist
-candi = 0
-for a in range(1, 100):
-    for b in range(1, 100):
-        digital_sum = sum(num2digitallist(a**b))
-        candi = digital_sum if candi < digital_sum else candi
-print(candi)
-print('The execution time is', time.clock()-T0)
-#972
-#The execution time is 1.34972735127
+from ProjectEulerCommons.Base import *
+
+Answer(
+    max([sum([int(digit) for digit in str(a**b)]) for a in range(100) for b in range(100)])
+)
+
+"""
+------------------------------------------------
+   ProjectEuler.Problem.056.py
+   The Answer is: 972
+   Time Elasped: 0.2762727737426758sec
+------------------------------------------------
+"""
